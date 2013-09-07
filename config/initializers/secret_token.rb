712ad5db9a93032ea29fbba3d85fefcdc6d70aa7
Ana Martinez <amartinez@zendesk.com>
2013-09-07 00:04:02 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Enefele::Application.config.secret_key_base = 'b34cbd319bc9ed25d2a839aae3d20515e0fe4c078151caa73f1ad9ab6a9c852035a8468da5de2a910479649370a0ffb28c02b797aed29de941d82ac144ba7580'
